67585bb9c8571c874de19b239d1096eeb4f009a5
wuno <ndiaz@wuno.com>
2016-12-04 05:37:04 -0600

    updated profile forms working / regex refining needed

--- assets/app/secure/profile.component.ts
+++ assets/app/secure/profile.component.ts
@@ -4,17 +4,18 @@ import { Http, Response, Headers }          from '@angular/http';
 import { Auth }                             from './../services/auth.service';
 import { Personal }    			            from '../models/personal';
 import { Address }    			            from '../models/address';
-import {FormGroup, Validators, FormControl} from "@angular/forms";
+import {FormGroup, Validators, FormControl, FormBuilder} from "@angular/forms";
 
 @Component({
     providers: [ Auth ],
     templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
-    constructor( private router: Router, private auth: Auth, private http: Http  ) { }
+    constructor(private _fb: FormBuilder, private router: Router, private auth: Auth, private http: Http) {
+    }
 
     //Personal Form Control
-    public personalSubmitted : Boolean = false;
+    public personalSubmitted: Boolean = false;
     personalForm: FormGroup;
     personal = new Personal(this.auth.user.email, this.auth.user.first_name, this.auth.user.middle_name, this.auth.user.last_name, '', '', '', '', '', '');
 
@@ -24,26 +25,14 @@ export class ProfileComponent implements OnInit {
         if (!this.personalForm.valid) {
             console.log('Personal Form Has Been Submitted but is not valid');
         } else {
-            const user = new Personal(
-                this.personalForm.value.email,
-                this.personalForm.value.first_name,
-                this.personalForm.value.middle_name,
-                this.personalForm.value.last_name,
-                this.personalForm.value.dob,
-                this.personalForm.value.mobile_phone,
-                this.personalForm.value.home_phone,
-                this.personalForm.value.business_phone,
-                this.personalForm.value.fax_number,
-                this.personalForm.value.ssn
-            )
             let headers = new Headers();
             headers.append('Content-Type', 'application/json');
             return this.http
                 .post('http://localhost:4200/api/updateProfile',
                     this.personal,
-                    { headers: headers })
-                .map((res:Response) => res.json())
-                .subscribe((res)=>{
+                    {headers: headers})
+                .map((res: Response) => res.json())
+                .subscribe((res) => {
                     //do something with the response here
                     console.log(res);
                 });
@@ -64,32 +53,62 @@ export class ProfileComponent implements OnInit {
         return this.http
             .post('http://localhost:4200/api/addAddress',
                 this.address,
-                { headers: headers })
+                {headers: headers})
             .subscribe();
     }
 
     addressActive = true;
-    states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+    states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
     ngOnInit() {
         this.personalForm = new FormGroup({
-            email : new FormControl(null,[
+            email : new FormControl(this.auth.user.email,Validators.required ),
+            first_name: new FormControl(null,[
                 Validators.required,
-                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-
+                Validators.pattern("^[^±!@£$%^&*_+§¡€#¢§¶•ªº«\\/<>?:;|=.,]{1,20}$")
             ]),
-            first_name : new FormControl (null,Validators.required,Validators.pattern("/^[a-z0-9]+$/i")),
-            middle_name : new FormControl (null,Validators.required,Validators.pattern("/^[a-z0-9]+$/i")),
-            last_name : new FormControl (null,Validators.required,Validators.pattern("/^[a-z0-9]+$/i")),
-            dob : new FormControl (null,Validators.required),
-            mobile_phone : new FormControl (null,Validators.required,Validators.pattern("/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/")),
-            home_phone : new FormControl (null,Validators.required,Validators.pattern("/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/")),
-            business_phone : new FormControl (null,Validators.required,Validators.pattern("/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/")),
-            fax_number : new FormControl (null,Validators.required,Validators.pattern("/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/")),
-            ssn : new FormControl (null,Validators.required,Validators.pattern("/^\d{3}-?\d{2}-?\d{4}$/")),
-        })
+            middle_name: new FormControl(null,[
+                Validators.required,
+                Validators.pattern("^[^±!@£$%^&*_+§¡€#¢§¶•ªº«\\/<>?:;|=.,]{1,20}$")
+            ]),
 
+            last_name: new FormControl(null,[
+                Validators.required,
+                Validators.pattern("^[^±!@£$%^&*_+§¡€#¢§¶•ªº«\\/<>?:;|=.,]{1,20}$")
+            ]),
+
+            dob : new FormControl (null, Validators.required ),
+            mobile_phone: new FormControl(null,Validators.required ),
+            home_phone: new FormControl(null,Validators.required ),
+
+            business_phone: new FormControl(null,Validators.required ),
+            fax_number: new FormControl(null,Validators.required ),
+            ssn: new FormControl(null,Validators.required )
+        });
+    }
+
+    save(model: Personal, isValid: boolean) {
+        if (!isValid) {
+            console.log('Personal Form is not valid');
+            console.log(model, isValid);
+        } else {
+            console.log('Personal Form is valid');
+            console.log(model, isValid);
+            let headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            return this.http
+                .post('http://localhost:4200/api/updateProfile',
+                    model,
+                    {headers: headers})
+                .map((res: Response) => res.json())
+                .subscribe((res) => {
+                    //do something with the response here
+                    console.log(res);
+                });
+        }
     }
 }
 
-// zip : new FormControl (null,Validators.required,Validators.pattern("/^\d{5}$/")),
+
+
+
