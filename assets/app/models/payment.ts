7a53cd79d7d600ac49186c405f8555ae683fcb6f
wuno <ndiaz@wuno.com>
2016-12-11 03:52:28 -0600

    Updated cc form / Added toast to cc form / lacking validation regex

--- assets/app/models/payment.ts
+++ assets/app/models/payment.ts
@@ -1,10 +1,12 @@
 export class Payment {
-  constructor(
-    public cardName: string,
-    public cardType: string,
-    public cardNumber: string,
-    public expMonth: string,
-    public expYear: string,
-    public cvc: string
+    constructor(
+        public id: string,
+        public email: string,
+        public cardName: string,
+        public cardType: string,
+        public cardNumber: string,
+        public expMonth: string,
+        public expYear: string,
+        public cvc: string
   ) {  }
 }
